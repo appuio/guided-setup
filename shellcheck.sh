5eeed91053ab03fa16f2702b841f8e887b8963cc
Aline Abler <aline.abler@vshn.ch>
2026-04-16 12:34:39 +0200

    Rename steps to spells

--- shellcheck.sh
+++ shellcheck.sh
@@ -22,27 +22,27 @@ shift $((OPTIND-1))
 
 script=$(mktemp)
 errors_found=0
-for stepfile in "${@}"
+for spellbook in "${@}"
 do
-    echo $stepfile
-    array_length=$( yq e ".steps | length - 1" "$stepfile" )
+    echo $spellbook
+    array_length=$( yq e ".spells | length - 1" "$spellbook" )
 
     if [ "$array_length" -lt 0 ] ; then
-        echo "Warning: Empty step file $stepfile"
+        echo "Warning: Empty spell file $spellbook"
     else
         for element_index in $( seq 0 "$array_length" );do
-            step="$( yq e ".steps[$element_index].match" "$stepfile" )"
+            spell="$( yq e ".spells[$element_index].match" "$spellbook" )"
 	    echo '# shellcheck disable=2034' > "$script"
-            echo "GANDALF_STEPFILE_DIR=baz" >> "$script"
-            yq e ".steps[$element_index].inputs[] | \"INPUT_\(.name)=foo\"" "$stepfile" | grep -v "INPUT_=foo" >> "$script"
-            echo "$step" | grep -oP '\?P<(.+?)>' | sed -nE 's#\?P<(.+)>#MATCH_\1=bar#p' >> "$script"
-            yq e ".steps[$element_index].run" "$stepfile" >> "$script"
+            echo "GANDALF_SPELLBOOK_DIR=baz" >> "$script"
+            yq e ".spells[$element_index].inputs[] | \"INPUT_\(.name)=foo\"" "$spellbook" | grep -v "INPUT_=foo" >> "$script"
+            echo "$spell" | grep -oP '\?P<(.+?)>' | sed -nE 's#\?P<(.+)>#MATCH_\1=bar#p' >> "$script"
+            yq e ".spells[$element_index].run" "$spellbook" >> "$script"
             if $verbose
             then
                 echo "###################################"
-                echo "### FILE: $stepfile"
+                echo "### FILE: $spellbook"
                 echo "### INDEX: $element_index"
-                echo "### STEP: $step"
+                echo "### SPELL: $spell"
                 echo "###################################"
             fi
             shellcheck -s bash "$script" -e 2129 -e 2002
@@ -51,7 +51,7 @@ do
             if (( ret != 0 )) && ! $verbose
             then
                 echo
-                echo " ^^^ Above errors found in $stepfile, step $element_index: '$step'"
+                echo " ^^^ Above errors found in $spellbook, spell $element_index: '$spell'"
             fi
             if $wait_for_user
             then
